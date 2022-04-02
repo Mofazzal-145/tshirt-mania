3838a3d09acb6b8cec76dc22a49ae88aa6ef6e4f
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-03 02:12:47 +0600

    add some comment

--- src/components/GrandPa/GrandPa.js
+++ src/components/GrandPa/GrandPa.js
@@ -4,6 +4,19 @@ import Uncle from '../Uncle/Uncle'
 import Aunty from '../Aunty/Aunty'
 import './GrandPa.css'
 
+/* 
+context api
+1. call createContext with a default value
+2. set a variable of the context with uppercase
+3. Make sure you export the context to use it in other places
+4. Wrap you child content using RingContext.Provider
+5. Provide a value
+6. To consume the context from child component
+7. useContext hook and pass the contextName.
+
+*/
+
+
 export const RingContext = createContext('diamond')
 
 const GrandPa = () => {
