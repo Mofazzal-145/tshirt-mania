3ee2c10b16a17d3b30818e9ecd1c35a949c2150b
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-02 18:18:17 +0600

    modified components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
 import Cart from '../Cart/Cart';
 import useTShirts from '../../hooks/useTShirts';
 import TShirt from '../TShirt/TShirt';
 const Home = () => {
     const [tShirts, setTShirts] = useTShirts();
+    const [cart, setCart] = useState([]);
     return (
         <div className='home-container'>
             <div className="tshirt-container">
